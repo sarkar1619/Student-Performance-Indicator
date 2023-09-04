d1e66d0c68554984c87e022a4a74cbb644a6ec1d
Pritam Sarkar <pritamps2002@gmail.com>
2023-09-04 23:52:14 +0530

    Started with UI Implementation

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,7 @@
-import pickle
 from flask import Flask, request, render_template
-import pandas as pd
-import numpy as np
-
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application = Flask(__name__)
-
-app = application
+app = Flask(__name__)
 
 ## Route for a home page
 @app.route('/')
@@ -37,5 +30,6 @@ def predict_datapoint():
 
         return render_template('home.html', results=results[0])
 
+# Debugging
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ Backend/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source "venv\Scripts\activate"
+python app.py
